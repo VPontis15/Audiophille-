2a627e14d1dd1891153907ac218ca403340230e5
Vassilis Pontis <vpontis15@gmail.com>
2025-03-17 19:04:14 +0200

    add ProductTypes interface for product and gallery structure

---
+++ src/types/ProductTypes.ts
@@ -0,0 +1,50 @@
+export interface RelatedProductProps {
+  name: string;
+  img?: string;
+  slug: string;
+  category?: string;
+}
+
+export interface ImageType {
+  mobile: string;
+  tablet: string;
+  desktop: string;
+}
+
+export interface GalleryType {
+  first: ImageType;
+  second: ImageType;
+  third: ImageType;
+}
+
+export interface IncludeType {
+  quantity: number;
+  item: string;
+}
+
+export interface RelatedProductType {
+  slug: string;
+  name: string;
+  image: ImageType;
+}
+
+export interface ProductProps {
+  id: number;
+  slug: string;
+  name: string;
+  image: ImageType;
+  category: string;
+  categoryImage: ImageType;
+  new: boolean;
+  price: number;
+  description: string;
+  features?: string;
+  includes?: IncludeType[];
+  gallery?: GalleryType;
+  others?: RelatedProductType[];
+  img?: string; // Keeping this for backward compatibility
+}
+
+export interface ProductOverviewProps {
+  product: ProductProps;
+}
