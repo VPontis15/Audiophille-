aa90644c21120c74602ad1dce2f38ba9aeb9c0dc
Vassilis Pontis <vpontis15@gmail.com>
2025-03-18 23:31:03 +0200

    add checkout button to CartFooter for improved user navigation

--- src/components/Cart/CartFooter.tsx
+++ src/components/Cart/CartFooter.tsx
@@ -1,14 +1,20 @@
 import { useAppSelector } from '../../types/hooks';
 import { ReactElement } from 'react';
+import Button from '../utils/Button';
 
 export default function CartFooter(): ReactElement | null {
   const totalCartPrice = useAppSelector((state) =>
     state.cart.items.reduce((acc, item) => acc + item.total, 0)
   );
   return totalCartPrice > 0 ? (
-    <div className="flex items-center justify-between">
-      <h6 className=" text-base uppercase opacity-50">Total</h6>
-      <h6 className="text-h6 font-bold ">${totalCartPrice}</h6>
-    </div>
+    <>
+      <div className="flex items-center justify-between">
+        <h6 className=" text-base uppercase opacity-50">Total</h6>
+        <h6 className="text-h6 font-bold ">${totalCartPrice}</h6>
+      </div>
+      <Button className="text-center" to="/checkout" primary>
+        Checkout
+      </Button>
+    </>
   ) : null;
 }
