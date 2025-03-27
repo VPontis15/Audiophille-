a631a4509f91a03f5e3b14f376e2facb2f7275b9
Vassilis Pontis <vpontis15@gmail.com>
2025-03-27 23:21:36 +0200

    refactor Logo component to use Link for navigation

--- client/src/components/utils/Logo.tsx
+++ client/src/components/utils/Logo.tsx
@@ -1,9 +1,13 @@
 import logo from '../../assets/logo.svg';
+import Link from './Link';
 
 export default function Logo(): React.ReactElement {
   return (
-    <div>
+    <Link
+      href="/"
+      className="flex-1 flex items-center justify-center sm:justify-start"
+    >
       <img src={logo} alt="audiophille logo" />
-    </div>
+    </Link>
   );
 }
