5f33a02e5def096697ba1f62ead4ffd0916a2fd0
Vassilis Pontis <vpontis15@gmail.com>
2025-03-27 23:21:57 +0200

    add MobileButton to Header for improved mobile navigation

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,13 +5,15 @@ import CartBtn from './utils/CartBtn';
 
 import Cart from './Cart/Cart';
 import { useAppSelector } from '../types/hooks';
+import MobileButton from './utils/MobileButton';
 
 export default function Header(): React.ReactElement {
   const isOpen = useAppSelector((state) => state.settings.isOpen);
   return (
     <header className="relative ">
       <div className="max-w-container border-b border-b-white/50  mx-auto flex justify-between py-8">
-        <div>
+        <div className="flex flex-1 items-center gap-4">
+          <MobileButton />
           <Logo />{' '}
         </div>
         <Navigation />
