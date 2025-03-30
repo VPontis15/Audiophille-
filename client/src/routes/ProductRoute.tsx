92016407422084431c2043e269d772c6a16299e1
Vassilis Pontis <vpontis15@gmail.com>
2025-03-30 14:12:41 +0300

    refactor import path for ProductLayout to improve code organization

--- client/src/routes/ProductRoute.tsx
+++ client/src/routes/ProductRoute.tsx
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router';
 import data from '../data/data.json';
 
-import ProductLayout from '../ProductLayout';
+import ProductLayout from '../layouts/ProductLayout';
 import ProductOverview from '../components/ProductOverview/ProductOverview';
 import { ProductProps } from '../types/ProductTypes';
 import Features from '../components/ProductOverview/Features';
