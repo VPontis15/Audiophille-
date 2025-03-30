80ce3942a362c24a96e3ecb1d2ee8df1bbe84419
Vassilis Pontis <vpontis15@gmail.com>
2025-03-30 14:13:02 +0300

    enhance Logo component to accept additional props for improved customization

--- client/src/components/utils/Logo.tsx
+++ client/src/components/utils/Logo.tsx
@@ -1,13 +1,17 @@
 import logo from '../../assets/logo.svg';
 import Link from './Link';
 
-export default function Logo(): React.ReactElement {
+interface LogoProps {
+  [key: string]: any;
+}
+
+export default function Logo(props: LogoProps): React.ReactElement {
   return (
     <Link
       href="/"
       className="flex-1 flex items-center justify-center sm:justify-start"
     >
-      <img src={logo} alt="audiophille logo" />
+      <img {...props} src={logo} alt="audiophille logo" />
     </Link>
   );
 }
