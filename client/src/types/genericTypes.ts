a30e426296a07666adecb0634feb2c1d504ecb47
Vassilis Pontis <vpontis15@gmail.com>
2025-04-03 00:42:09 +0300

    add generic ButtonProps type for enhanced button component flexibility

---
+++ client/src/types/genericTypes.ts
@@ -0,0 +1,12 @@
+export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
+  Partial<{
+    outline?: boolean;
+    rounded?: boolean;
+    to?: string;
+    children: React.ReactNode;
+    primary?: boolean;
+    secondary?: boolean;
+    transparent?: boolean;
+    sm?: boolean;
+    lg?: boolean;
+  }>;
