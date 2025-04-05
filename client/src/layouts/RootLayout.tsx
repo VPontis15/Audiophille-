349366be8bb5180949996e46f29ff87e8cc9d605
Vassilis Pontis <vpontis15@gmail.com>
2025-04-05 23:45:51 +0300

    add ScrollToTop component to DashboardLayout and RootLayout for improved navigation experience

--- client/src/layouts/RootLayout.tsx
+++ client/src/layouts/RootLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import ScrollToTop from '../components/components/ui/ScrollToTop';
 
 export default function RootLayout(): React.ReactElement {
   return (
     <div className="relative z-10 font-sans leading-[1.7] text-text text-main bg-body mx-auto ">
+      <ScrollToTop />
       <Header />
       <Outlet />
       <Footer />
