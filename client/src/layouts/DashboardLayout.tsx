8e74c535e14ed4a509d112a565be0bb1b0066d6c
Vassilis Pontis <vpontis15@gmail.com>
2025-04-07 22:52:23 +0300

    refactor: remove ScrollToTop component from DashboardLayout for cleaner structure

--- client/src/layouts/DashboardLayout.tsx
+++ client/src/layouts/DashboardLayout.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import { Outlet } from 'react-router';
 import Navigation from '../components/Dashboard/Navigation/Navigation';
 import DashboardSectionLayout from './DashboardSectionLayout';
-import ScrollToTop from '../components/components/ui/ScrollToTop';
 
 export default function DashboardLayout(): React.ReactElement {
   return (
     <main className="grid grid-cols-[14rem_1fr] min-h-screen">
-      <ScrollToTop />
       <Navigation />
       <DashboardSectionLayout>
         <Outlet />
