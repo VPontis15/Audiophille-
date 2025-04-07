0d76c6d732ea606926048aa8227da1503755e6a5
Vassilis Pontis <vpontis15@gmail.com>
2025-04-07 22:49:41 +0300

    add category and brand routes to the API for enhanced product management

--- backend/app.js
+++ backend/app.js
@@ -5,11 +5,14 @@ const app = express();
 const fs = require('fs');
 const productRouter = require('./routes/productRoutes');
 const orderRouter = require('./routes/orderRoutes');
-
+const categoryRouter = require('./routes/categoriesRoutes');
+const brandRouter = require('./routes/brandsRoutes');
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1/products', productRouter);
 app.use('/api/v1/orders', orderRouter);
+app.use('/api/v1/categories', categoryRouter);
+app.use('/api/v1/brands', brandRouter);
 module.exports = app;
