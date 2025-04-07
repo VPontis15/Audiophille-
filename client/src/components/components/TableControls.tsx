b534f3d653603efc9848a881d57ed0f9f91ccbd6
Vassilis Pontis <vpontis15@gmail.com>
2025-04-07 22:51:54 +0300

    feat: add optional add button to TableControls component with routing support

--- client/src/components/components/TableControls.tsx
+++ client/src/components/components/TableControls.tsx
@@ -1,26 +1,38 @@
+import { createPortal } from 'react-dom';
 import { TableControlsProps } from '../../types/Dashboard/types';
+import Button from '../utils/Button';
 import SelectInput from './ui/SelectInput';
 import { IoMdSearch } from 'react-icons/io';
-
+import { IoMdAdd } from 'react-icons/io';
+import { Outlet } from 'react-router';
 export default function TableControls({
   options,
   currentLimit,
+  addLink = '',
   onLimitChange,
   onSearchChange,
+  addBtn = false,
 }: TableControlsProps) {
   return (
     <div className="flex justify-between items-center my-4 py-2">
-      <div className="flex items-center gap-2">
-        <span className="text-sm text-gray-600">Show:</span>
-        <SelectInput
-          options={options}
-          name="limit"
-          value={String(currentLimit)}
-          onChange={(e) => onLimitChange(e.target.value)}
-        />
-        <span className="text-sm text-gray-600">entries</span>
+      <div className="flex gap-4 items-center">
+        <div className="flex items-center gap-2">
+          <span className="text-sm text-gray-600">Show:</span>
+          <SelectInput
+            options={options}
+            name="limit"
+            value={String(currentLimit)}
+            onChange={(e) => onLimitChange(e.target.value)}
+          />
+          <span className="text-sm text-gray-600">entries</span>
+        </div>
+        {addBtn && (
+          <Button to={addLink} sm withIcon className=" text-accent rounded-md">
+            <IoMdAdd width={20} height={20} className="w-5 h-5" />
+            Add New
+          </Button>
+        )}
       </div>
-
       <div className="relative">
         <input
           type="text"
@@ -34,6 +46,7 @@ export default function TableControls({
           className="absolute w-5 h-5 left-2 top-1/2 transform -translate-y-1/2 text-gray-500"
         />
       </div>
+      {createPortal(<Outlet />, document.body)}
     </div>
   );
 }
