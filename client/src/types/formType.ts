95e83fc0f996a2d4b316941e1883b7d6fce6cdcf
Vassilis Pontis <vpontis15@gmail.com>
2025-04-12 21:57:25 +0300

    feat: update FormInputProps to allow more flexible value types

--- client/src/types/formType.ts
+++ client/src/types/formType.ts
@@ -3,7 +3,7 @@ export interface FormInputProps {
   name: string;
   type: string;
   placeholder?: string;
-  value: string;
+  value: string | number | boolean | Date | null | File | File[] | string[];
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   required?: boolean;
   full?: boolean;
